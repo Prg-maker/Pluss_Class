0931b08c29db7579a1a6278c1a2358aa6c563f1f
Prg-maker <df3296709@gmail.com>
2022-04-18 12:00:12 -0300

    new config

--- front/src/models/PagesLogin/PageLogin/index.tsx
+++ front/src/models/PagesLogin/PageLogin/index.tsx
@@ -3,22 +3,12 @@ import { Container, Title, Button, IconGoogle, IconGithub } from './styles'
 import PersonPng from './assents/person.png'
 import PlussClassSvg from './assents/Pluss+Class_Logo.svg'
 
-import {GoogleAuthProvider , getAuth} from 'firebase/auth'
-
 
 
 export function PageLogin() {
 
 
-  const provider = new GoogleAuthProvider()
-  const auth = getAuth()
 
-  async function handleSingIn(){
-
-    
-
-
-  }
 
 
   return (
@@ -31,7 +21,7 @@ export function PageLogin() {
 
         <Title>Entrar com...</Title>
 
-        <Button onClick={handleSingIn}>
+        <Button >
           <IconGoogle />
           Google
         </Button>
